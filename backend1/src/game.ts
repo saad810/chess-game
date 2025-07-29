40244d519edfc24edb4949ebb13f3fa147385306
saad810 <saadameen810@gmail.com>
2025-07-29 23:06:02 +0500

    backend update

--- backend1/src/game.ts
+++ backend1/src/game.ts
@@ -1,18 +1,17 @@
 import { WebSocket } from "ws";
+import { Chess } from "chess.js";
 
 export class Game {
     public player1: WebSocket;
     public player2: WebSocket;
-    public board: string;
-    public moves: string[];
+    public board: Chess;
     public startTime: Date;
 
 
     constructor(player1: WebSocket, player2: WebSocket) {
         this.player1 = player1;
         this.player2 = player2;
-        this.board = "";
-        this.moves = [];
+        this.board = new Chess();
         this.startTime = new Date();
     }
 
