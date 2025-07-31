b51afc00f35f26350f9227f75ec05837cef69486
saad810 <saadameen810@gmail.com>
2025-07-31 21:19:58 +0500

    chess board update

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,20 +1,39 @@
 import type { Color, PieceSymbol, Square } from "chess.js"
+import { useState } from "react";
 
-const ChessBoard = ({ board }: {
+const ChessBoard = ({ board, socket }: {
   board: ({
     square: Square;
     color: Color;
     type: PieceSymbol
-  } | null)[][]
+  } | null)[][];
+  socket: WebSocket | null;
 }) => {
-  console.log(board)
+  // console.log(board)
+  console.log("[board]:", board);
+  const [from, setFrom] = useState<Square | null>(null);
+  const [to, setTo] = useState<Square | null>(null);
   return (
     <div>
       {
         board.map((row, rowIndex) => (
           <div key={rowIndex} className="flex">
             {row.map((square, colIndex) => (
-              <div key={colIndex} className={`w-16 h-16 flex items-center justify-center ${((rowIndex + colIndex) % 2 === 0) ? 'bg-amber-50' : 'bg-primary'}`}>
+              <div onClick={
+                () => {
+                  // Handle square click logic here
+                  console.log(`Square clicked: Row ${rowIndex}, Col ${colIndex}`, square);
+                  if(!from) {
+                    setFrom(square?.square ?? null);
+                    console.log("From:", square?.square);
+                  }else{
+                    setTo(square?.square ?? null);
+                    console.log("To:", square?.square);
+                    
+                  }
+
+                }
+              } key={colIndex} className={`w-16 h-16 flex items-center justify-center ${((rowIndex + colIndex) % 2 === 0) ? 'bg-amber-50' : 'bg-primary'}`}>
                 {square ? (
                   <span >{square.type}</span>
                 ) : null}
